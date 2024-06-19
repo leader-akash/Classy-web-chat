3bf311fc352ddb106f2cb127f62da8453fcaa76c
it <leader.akashak@gmail.com>
2024-06-19 10:06:18 +0530

    created new contact sheet, new chat sheet, updated api for new contact

--- src/app/_components/sidebar/sheets/NewChatSheet.tsx
+++ src/app/_components/sidebar/sheets/NewChatSheet.tsx
@@ -1,11 +1,20 @@
 "use client";
 import { User } from "@prisma/client";
-import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import Image from "next/image";
 import { ArrowLeft } from "lucide-react";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import NewContactSheet from "./NewContactSheet";
 
 interface NewChatSheetProps {
   currentUser: User & {
@@ -14,9 +23,23 @@ interface NewChatSheetProps {
 }
 
 const NewChatSheet = ({ currentUser }: NewChatSheetProps) => {
+  const [searchText, setSearchText] = useState<string>("");
+  const [isModelOpen, setIsModalOpen] = useState(false);
+  const [contacts, setContacts] = useState<User[]>([])
 
-    const [searchText, setSearchText] = useState<string>('');
-    const [isModelOpen, setIsModalOpen] = useState(false)
+  useEffect(()=>{
+    if(currentUser.following !== undefined){
+      setContacts(currentUser?.following)
+    }
+  },[currentUser])
+
+  const handleAddContact = (contacts: User[]) => {
+    setContacts(contacts)
+  }
+
+  const handleRemoveContact = (newContacts: User[]) => {
+    setContacts(contacts)
+  }
 
   return (
     <div>
@@ -24,7 +47,6 @@ const NewChatSheet = ({ currentUser }: NewChatSheetProps) => {
 
       <Sheet>
         <SheetTrigger>
-          <button>
             <Image
               className="hover:cursor-pointer "
               src={"/images/NewChat.svg"}
@@ -32,37 +54,49 @@ const NewChatSheet = ({ currentUser }: NewChatSheetProps) => {
               width={24}
               height={24}
             />
-          </button>
         </SheetTrigger>
         <SheetContent side="left" className="w-[340px] sm:w-540px] p-0">
-                
-        <SheetHeader className="bg-primary">
-          <div className="flex mt-14 mb-3 items-center">
-            <SheetClose asChild>
-              <ArrowLeft className="mr-7 ml-5 cursor-pointer text-white" />
-            </SheetClose>
-            <SheetTitle className="text-white fle items-center justify-center">
-              New Chat
-            </SheetTitle>
-          </div>
-        </SheetHeader>
+          <SheetHeader className="bg-primary">
+            <div className="flex mt-14 mb-3 items-center">
+              <SheetClose asChild>
+                <ArrowLeft className="mr-7 ml-5 cursor-pointer text-white" />
+              </SheetClose>
+              <SheetTitle className="text-white fle items-center justify-center">
+                New Chat
+              </SheetTitle>
+            </div>
+          </SheetHeader>
 
-            <ScrollArea >
-                <div className="space-y-2 flex">
-                <div className="flex bg-gray-100 w-11/12 m-auto rounded-xl mt-2 ml-3">
-                    <button onClick={()=> setSearchText("")}>
-                        <ArrowLeft className="text-primary mr-7 ml-5"/>
-                    </button>
-                    
-                    <Input placeholder="Search name or number"
-                        className="bg-transparent border-0"
-                        value={searchText}
-                        onChange={(e) => setSearchText(e.target.value)}
-                    />
-                </div>
-                </div>
-            </ScrollArea>
+          <ScrollArea>
+            <div className="space-y-2 flex">
+              <div className="flex bg-gray-100 w-11/12 m-auto rounded-xl mt-2 ml-3">
+                <button onClick={() => setSearchText("")}>
+                  <ArrowLeft className="text-primary mr-7 ml-5" />
+                </button>
 
+                <Input
+                  placeholder="Search name or number"
+                  className="bg-transparent border-0"
+                  value={searchText}
+                  onChange={(e) => setSearchText(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <button className="flex m-6 relative justify-center items-center">
+              <Avatar  className="flex w-12 h-12"> 
+                <AvatarImage src="/images/Group.svg" className="" />
+
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+
+              <div className="ml-4 text-left">
+                <NewContactSheet 
+                  handleAddContact={handleAddContact}
+                />
+              </div>
+            </button>
+          </ScrollArea>
         </SheetContent>
       </Sheet>
     </div>
